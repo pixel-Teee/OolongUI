6e7425783100f40a09f1717032887f855d5957f1
pixel-Teee <47231777+pixel-Teee@users.noreply.github.com>
2025-12-01 11:40:25 +0800

    create sdl callback to create window

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,38 @@
-#include <SDL3/SDL_log.h>
+//SDL_MAIN_USE_CALLBACKS告知SDL_main.h去使用SDL回调作为入口
+#define SDL_MAIN_USE_CALLBACKS
+#include <SDL3/SDL_main.h>
+#include <SDL3/SDL.h>
 
-int main()
+SDL_Window* window;
+
+SDL_AppResult SDL_AppInit(void** appstate, int argc, char** argv)
 {
-	//entry point
-	SDL_LogInfo(SDL_LOG_CATEGORY_APPLICATION, "entry point");
+	//create a window
+	window = SDL_CreateWindow("Hello, Triangle!", 960, 540, SDL_WINDOW_RESIZABLE);
 
-	while (1)
+	return SDL_APP_CONTINUE;
+}
+
+//update回调
+SDL_AppResult SDL_AppIterate(void* appstate)
+{
+	return SDL_APP_CONTINUE;
+}
+
+SDL_AppResult SDL_AppEvent(void* appstate, SDL_Event* event)
+{
+	// close the window on request
+	if (event->type == SDL_EVENT_WINDOW_CLOSE_REQUESTED)
 	{
-		//nothing
-		//SDL_LogInfo(SDL_LOG_CATEGORY_APPLICATION, "entry point");
+		return SDL_APP_SUCCESS;
 	}
-	return 0;
+
+	return SDL_APP_CONTINUE;
+}
+
+void SDL_AppQuit(void* appstate, SDL_AppResult result)
+{
+	// destroy the window
+	SDL_DestroyWindow(window);
 }
 
